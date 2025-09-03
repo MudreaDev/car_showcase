0917491f03748f8bd24d5043638b0040b3fa9058
Mihai <mihail.mudrea@isa.utm.md>
2025-09-03 13:55:21 +0300

    adaugare footer navbar, start add Filter

--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,10 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
-    handlecClick?: MouseEventHandler<HTMLButtonElement>;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+}
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
 }
